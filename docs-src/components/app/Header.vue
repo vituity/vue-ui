e34bb8c205e067f8bfcaccc832892aa37cf5b953
Monroe Gollaher <mgollaher92@gmail.com>
2024-08-27 15:21:24 -0600

    more progress

--- docs-src/components/app/Header.vue
+++ docs-src/components/app/Header.vue
@@ -1,42 +1,60 @@
-<template lang="pug">
-nav.navbar.is-dark(role='navigation' aria-label='main navigation')
-  .navbar-brand
-    router-link.navbar-item(to="/")
-      img(src='@/assets/logo.png' height='28')
-  #navbarBasicExample.navbar-menu
-    .navbar-start
-      a.navbar-item
-        | Home
-      a.navbar-item
-        | Documentation
-      .navbar-item.has-dropdown.is-hoverable
-        a.navbar-link
-          | More
-        .navbar-dropdown
-          a.navbar-item
-            | About
-          a.navbar-item
-            | Jobs
-          a.navbar-item
-            | Contact
-          hr.navbar-divider
-          a.navbar-item
-            | Report an issue
-    .navbar-end
-      .navbar-item
-        .b-btns
-          a.b-btn.is-primary
-            strong Sign up
-          a.b-btn.is-info
-            | Log in
+<template>
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link class="navbar-item" to="/">
+        <img src="@/assets/logo.png" height="28" alt="Logo"/>
+      </router-link>
+    </div>
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" href="#">
+          Home
+        </a>
+        <a class="navbar-item" href="#">
+          Documentation
+        </a>
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link" href="#">
+            More
+          </a>
+          <div class="navbar-dropdown">
+            <a class="navbar-item" href="#">
+              About
+            </a>
+            <a class="navbar-item" href="#">
+              Jobs
+            </a>
+            <a class="navbar-item" href="#">
+              Contact
+            </a>
+            <hr class="navbar-divider"/>
+            <a class="navbar-item" href="#">
+              Report an issue
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="b-btns">
+            <a class="b-btn is-primary" href="#">
+              <strong>Sign up</strong>
+            </a>
+            <a class="b-btn is-info" href="#">
+              Log in
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
   data() {
     return {
-
-    }
+    };
   }
 }
 </script>
--- docs-src/components/app/Page.vue
+++ docs-src/components/app/Page.vue
@@ -1,42 +1,64 @@
-<template lang="pug">
-.page
-  .page--header
-    slot(name="header")
-      h3.title {{title}} <small>{{subTitle}}</small>
-  slot
+<template>
+  <div class="page">
+    <div class="page--header">
+      <slot name="header">
+        <h3 class="title">{{ title }} <small>{{ subTitle }}</small></h3>
+      </slot>
+    </div>
+    <slot></slot>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ['title', 'subTitle']
-
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    subTitle: {
+      type: String,
+      default: ''
+    }
+  }
 }
 </script>
 
-<style lang="sass" scoped>
-.page
-  flex: 1
-  display: flex
-  flex-direction: column
-  .page--header
-    padding: 14px
-    display: flex
-    flex-direction: row
-    align-items: center
-    position: relative
-    height: 3rem
-    z-index: 1
-    flex: auto 0 0
-    > *
-      flex: auto 0 0
-    .title
-      font-size: 1.5rem
-      font-weight: lighter
-      margin-bottom: 0
-    .spacer
-      flex: auto 1 1
-      width: 0
-      height: 0
-  .page--body
-    padding: 10px
+<style scoped>
+.page {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+.page--header {
+  padding: 14px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: relative;
+  height: 3rem;
+  z-index: 1;
+  flex: auto 0 0;
+}
+
+.page--header > * {
+  flex: auto 0 0;
+}
+
+.title {
+  font-size: 1.5rem;
+  font-weight: lighter;
+  margin-bottom: 0;
+}
+
+.spacer {
+  flex: auto 1 1;
+  width: 0;
+  height: 0;
+}
+
+.page--body {
+  padding: 10px;
+}
 </style>
